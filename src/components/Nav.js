1ff74674c82744e39ad99282bfc2020bb5a1c5b1
Ryan-Yeo <ryanrj@hotmail.sg>
2020-08-07 16:55:30 +0800

    Add mobile responsivness to nav and search bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "../context";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Nav() {
   const context = useContext(DataContext);
+  const [navToggle, setNavToggle] = useState(false);
   const { favorites } = context;
   const { pathname } = useLocation();
 
@@ -17,21 +18,42 @@ export default function Nav() {
 
   return (
     <nav className="nav">
-      <Link className="navlogo" to="/">
-        Galler<span className="darkgrey">easy</span>
-      </Link>
-      <Link
-        className={`navlinks ${pathname === "/" ? active : notActive}`}
-        to="/"
+      <div className="nav-header">
+        <Link className="navlogo" to="/">
+          Galler<span className="darkgrey">easy</span>
+        </Link>
+        <button className="navbtn" onClick={() => setNavToggle(!navToggle)}>
+          <i
+            className={
+              navToggle
+                ? "menu bx bx-menu bx-sm bx-burst"
+                : "menu bx bx-menu bx-sm"
+            }
+          ></i>
+        </button>
+      </div>
+      <ul
+        className={navToggle ? "navlinks-holder" : "navlinks-holder hidelinks"}
       >
-        Search
-      </Link>
-      <Link
-        className={`navlinks ${pathname === "/favorites" ? active : notActive}`}
-        to="/favorites"
-      >
-        Favorites{favNum}
-      </Link>
+        <li>
+          <Link
+            className={`navlinks ${pathname === "/" ? active : notActive}`}
+            to="/"
+          >
+            Search
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`navlinks ${
+              pathname === "/favorites" ? active : notActive
+            }`}
+            to="/favorites"
+          >
+            Favorites{favNum}
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
